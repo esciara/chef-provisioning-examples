d38878e9d9cc0a708e4ad7c489d2f3c2ca782d79
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-12-25 08:49:02 +0100

    simplified aws setup file

--- aws_setup.rb
+++ aws_setup.rb
@@ -3,8 +3,5 @@ require 'chef/provisioning'
 # Needs to have the ~/.aws/config set up for it to work
 with_driver 'fog:AWS'
 with_machine_options :bootstrap_options => { 
-  :key_name => 'chefserver', 
-  :image_id => 'ami-60a10117', 
-  :flavor_id => 't2.micro', 
-  :vpc_id => 'vpc-6648a303', 
-  :subnet_id => 'subnet-0a935c6f' }
+  :key_name => 'chefserver'   # Change this to your own key pair
+}
