1a16ca8b11838b75d9774cafc26645ece5f50012
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2015-01-18 21:29:57 +0100

    increased memory on vagrant vms

--- vagrant_setup.rb
+++ vagrant_setup.rb
@@ -6,6 +6,10 @@ vagrant_box 'precise64' do
   url 'http://files.vagrantup.com/precise64.box'
 end
 
-with_machine_options :vagrant_options => {
-  'vm.box' => 'precise64'
-}
+with_machine_options :vagrant_config  => <<-ENDCONFIG
+config.vm.box = "precise64"
+config.vm.provider "virtualbox" do |v|
+  v.memory = 1024
+  v.cpus = 2
+end
+ENDCONFIG
